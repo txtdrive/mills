7a0df73bc22789f8bba3b2074751d322d30352cb
txtdrive <txtdrive@gmail.com>
2021-08-09 12:19:40 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('#fullPage').fullpage({
+    navigation: true,
+    anchors:['s1','s2','s3','s4'],
+    controlArrows:false,
+    slidesNavigation:true,
+    scrollOverflow:true,
+    navigationPosition:true,
+    touchSensitivity:8
+    //paddingBottom: '5rem'
+})
